908bb3e6346d11157a86f5385546fac77632c7b0
Andrew-Delgado <andrewdelgado017@gmail.com>
2019-12-30 18:31:50 -0800

    more practice

--- src/app.kt
+++ src/app.kt
@@ -1,25 +1,41 @@
-data class Person(val name: String,
-                  val age: Int? = null)
+//data class Person(val name: String,
+//                  val age: Int? = null)
+//
+//fun add(a: Int, b: Int): Int {
+//    return a + b
+//}
+//
+//fun printText(){
+//    println("FROM FUNCTIONNNN")
+//}
+//
+//fun main (args : Array<String>) {
+//    val persons = listOf(
+//        Person("Alice"),
+//        Person("Bob", age = 29)
+//    )
+//
+//    val oldest = persons.maxBy { it.age ?: 0 }
+//
+//    println("The oldest is: $oldest")
+//
+//    println("a plus b is: ${add(5,6)}")
+//
+//    printText()
+//}
+//
+//fun main(args: Array<String>){
+//    val name = if (args.isNotEmpty()) args[0] else "Kotlin"
+//    println("Hello, $name!")
+//
+//    println("Hello, ${if (args.isNotEmpty()) args[0] else "Kotlin"}!")
+//}
 
-fun add(a: Int, b: Int): Int {
-    return a + b
-}
+class Person(val name: String, var isMarried: Boolean)
 
-fun printText(){
-    println("FROM FUNCTIONNNN")
-}
+fun main(){
+    val p1 = Person("Andrew", false)
 
-fun main (args : Array<String>) {
-    val persons = listOf(
-        Person("Alice"),
-        Person("Bob", age = 29)
-    )
-
-    val oldest = persons.maxBy { it.age ?: 0 }
-
-    println("The oldest is: $oldest")
-
-    println("a plus b is: ${add(5,6)}")
-
-    printText()
+    p1.isMarried = true
+    println("Is ${p1.name} married?: ${p1.isMarried}")
 }
