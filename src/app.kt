839ac155c4a85840b05b8503d64accf082b5199c
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-01-02 20:58:09 -0800

    more practice

--- src/app.kt
+++ src/app.kt
@@ -31,18 +31,53 @@
 //    println("Hello, ${if (args.isNotEmpty()) args[0] else "Kotlin"}!")
 //}
 
-class Person(val name: String, var isMarried: Boolean){
-    val isAndrew: Boolean
-        get(){
-            return name == "Andrew"
-        }
+// package practicingKotlin
+
+//class Person(val name: String, var isMarried: Boolean){
+//
+//    // custom getter, since we are describing the properties, can also think of it as the state of the class
+//    val isAndrewAndMarried: Boolean
+//        get(){
+//            return name == "Andrew" && isMarried;
+//        }
+//
+//    fun work(){
+//        println("work function")
+//    }
+//}
+//
+//fun main(){
+//    val p1 = Person("Andrew", false)
+//
+//    p1.isMarried = true
+//    println("Is ${p1.name} married?: ${p1.isMarried}")
+//
+//    println("Is this andrew?: ${p1.isAndrewAndMarried}")
+//
+//    p1.work()
+//}
+
+import java.util.Random
+
+class Rectangle(val height: Int, val width: Int){
+    // Think of it as the state of the class.
+    val isSquare: Boolean
+        get() = height == width
+
+    override fun toString():String{
+        return "height: $height, width: $width"
+    }
+}
+
+fun randRectangle() : Rectangle{
+    val random = Random()
+    return Rectangle(random.nextInt(), random.nextInt())
 }
 
 fun main(){
-    val p1 = Person("Andrew", false)
+    val r1 = Rectangle(5,6)
+    val r2 = randRectangle()
 
-    p1.isMarried = true
-    println("Is ${p1.name} married?: ${p1.isMarried}")
-
-    println("Is this andrew?: ${p1.isAndrew}")
+    println(r1.width)
+    println("r1 is: $r1 \nr2 is: $r2")
 }
